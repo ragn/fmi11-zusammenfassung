4d09c67edde0fbbd8187b347bc6f89a37fe49616
Ralf Gnädinger <ralf.gnaedinger@gmail.com>
2014-07-13 20:18:37 +0200

    Umwandlung von NEA nach DEA

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @echo off
-set path=C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\
+set path=%localappdata%\MiKTeX 2.9\miktex\bin\
 set pdflatex=pdflatex.exe
 IF NOT EXIST "%path%" (
 set path="C:\Program Files\MiKTeX 2.9\miktex\bin\"
